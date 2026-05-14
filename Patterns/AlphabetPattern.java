bc8d2db8516514eac808d0695913a48980f871ec
YashBachwani <yash.bachwani30@gmail.com>
2026-05-14 16:24:10 +0530

    Alphabets Pattern Updated

---
+++ Patterns/AlphabetPattern.java
@@ -0,0 +1,13 @@
+public class AlphabetPattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 65; j < 65 + i; j++) {
+                System.out.print((char) j);
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns/SameAlpha.java
@@ -0,0 +1,16 @@
+public class SameAlpha {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+
+            // Character for current row
+            char ch = (char)(64 + i);
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+}
