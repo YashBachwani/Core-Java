7a74b3327599d1ff655f1a4d6c25408ba86ba0f2
YashBachwani <yash.bachwani30@gmail.com>
2026-05-16 22:37:25 +0530

    Hollow Triangle Updated

---
+++ Patterns/HollowTriangle.java
@@ -0,0 +1,22 @@
+public class HollowTriangle {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                // Print star at borders
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
