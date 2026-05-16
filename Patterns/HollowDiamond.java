d77f8f806f30b83c8d9029e3acb8a120f50de823
YashBachwani <yash.bachwani30@gmail.com>
2026-05-16 22:47:20 +0530

    Hollow Diamond Updated

---
+++ Patterns/HollowDiamond.java
@@ -0,0 +1,49 @@
+public class HollowDiamond {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print hollow stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print hollow stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
